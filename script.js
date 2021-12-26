a67360e73f54595c1031c1b4add0d0e33f80f3da
Slavik <bvarpl@gmail.com>
2021-12-26 18:34:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+/*var leftBorderWith = 1;
+{
+let second = 2;
+
+}
+const pi = 3.14;
+console.log(second);*/
+var string = "Hello";
+var number = 5;
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+console.log(4/0);
+console.log('string'*9);
+let somethin;
+console.log(somethin)
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpeg','apple.bmp'];
+
+console.log(arr[0])
+
+//alert("Hello Word");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = prompt("Are you here?", "Yes");
+console.log(typeof(arr));
